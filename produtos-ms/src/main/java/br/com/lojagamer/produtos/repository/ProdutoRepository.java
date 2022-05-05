7a13722605ee81755692b96e11c5990c07abd3c2
PedroVinícios2022 <53129287+PedroTheProgrammer@users.noreply.github.com>
2022-05-04 21:38:42 -0300

    Delete produtos-ms directory

--- produtos-ms/src/main/java/br/com/lojagamer/produtos/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.lojagamer.produtos.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import br.com.lojagamer.produtos.model.Produto;
-
-public interface ProdutoRepository extends MongoRepository<Produto, String> {
-    
-}
